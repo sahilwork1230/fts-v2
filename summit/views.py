8cd8665e3cdbd79dfc3d0f1924529ef552e2a0ae
sahilwork1230 <sahilwork1230@gmail.com>
2026-01-28 16:18:43 +0530

    implement tailwind reload and add static media files

--- summit/views.py
+++ summit/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sponsors(request):
+    return render(request, "sponsors.html")
+
+def speakers(request):
+    pass
--- tech/settings.py
+++ tech/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'summit',
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'tech.urls'
